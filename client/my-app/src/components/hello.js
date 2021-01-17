2199a9515ce27223339759030d0d2c62e3b16ec7
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-01-16 23:07:44 -0600

    Modify

--- client/my-app/src/components/hello.js
+++ client/my-app/src/components/hello.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import './Header/Header.css'
 
 
 /*      Conetion with Nodejs: no success */
 
-export const Header = ()=> {
+export const Header = () => {
     const [initialState, setInitialState] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('/api/').then(res => {
-            if(res.ok){
+            if (res.ok) {
                 return res.json()
             }
         }).then(jsonResponse => setInitialState(jsonResponse.hello))
-    },[])
+    }, [])
 
     console.log(initialState);
     return (
-    <header className="header">
-        <div>
-            <h1>Estados de la República</h1>
-            <ul>
-                <li><a href='#' onclick="console.log('The link was clicked.'); return false">Home</a></li>
-                <li><a href='#' onclick="console.log('The link was clicked.'); return false">Lista de Categorías</a></li>
-            </ul>
-        </div>
-    </header>
+        <header className="header">
+            <div>
+                <h1>Estados de la República</h1>
+                <div className="Grid">
+                    <ul>
+                        <li><a href='#' onclick="console.log('The link was clicked.'); return false">Home</a></li>
+                        <li><a href='#' onclick="console.log('The link was clicked.'); return false">Lista de Categorías</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
         /*<div>
             {initialState.length > 0 && initialState.map((e,i) => <li key={i}>{e}</li>)}
         </div>*/
